6b9685e84ad6eb16f19c6ee13274ac78d6bf522d
gharrington44 <146124932+gharrington44@users.noreply.github.com>
2026-05-22 18:30:12 -0400

    Update solution for matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -7,7 +7,7 @@ def matrix_transpose(A):
     # Write code here
     A = np.array(A)
     rows, cols = A.shape
-    new_arr = np.zeros((cols, rows))                      
+    new_arr = np.zeros((cols, rows), dtype=A.dtype)                      
 
     for i in range(rows):
         for j in range(cols):
